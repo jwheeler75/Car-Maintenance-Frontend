42bdd268700902a00979aaf4a00f1e183bbc5c82
Jason Wheeler <wheelerjasonj@johndeere.com>
2020-09-15 10:04:33 -0500

    css complete

--- src/CarDetail.js
+++ src/CarDetail.js
@@ -10,34 +10,44 @@ class CarDetail extends Component {
       <div className="App">
         <Link to="/">Back to Homepage</Link>
         <h3> {CarDetails.vehicle} </h3>
-        <h5>Last Oil Change Mileage: {CarDetails.lastOilChangeMileage}</h5>
-        <form onSubmit={this.props.changeLastOilMileage}>
-          <input type="hidden" name="carid" value={CarDetails.id} />
-          <input type="number" name="changeLastOilMileage" />
-          <input type="submit" value="Change Last Oil Change Mileage" />
-        </form>
-        <h5>Next Oil Change Mileage: {CarDetails.nextOilChangeMileage}</h5>
-        <form onSubmit={this.props.changeNextOilMileage}>
-          <input type="hidden" name="carid" value={CarDetails.id} />
-          <input type="number" name="changeNextOilMileage" />
-          <input type="submit" value="Change Next Oil Change Mileage" />
-        </form>
-        <h5>
-          Last Tire Rotation Mileage: {CarDetails.lastTireRotationMileage}
-        </h5>
-        <form onSubmit={this.props.changeLastRotation}>
-          <input type="hidden" name="carid" value={CarDetails.id} />
-          <input type="number" name="changeLastRotation" />
-          <input type="submit" value="Change Last Tire Rotation Mileage" />
-        </form>
-        <h5>
-          Next Tire Rotation Mileage: {CarDetails.nextTireRotationMileage}
-        </h5>
-        <form onSubmit={this.props.changeNextRotation}>
-          <input type="hidden" name="carid" value={CarDetails.id} />
-          <input type="number" name="changeNextRotation" />
-          <input type="submit" value="Change Next Tire Rotation Mileage" />
-        </form>
+        <div className="grid-container2">
+          <div className="grid">
+            <h5>Last Oil Change Mileage: {CarDetails.lastOilChangeMileage}</h5>
+            <form onSubmit={this.props.changeLastOilMileage}>
+              <input type="hidden" name="carid" value={CarDetails.id} />
+              <input type="number" name="changeLastOilMileage" />
+              <input type="submit" value="Change Last Oil Change Mileage" />
+            </form>
+          </div>
+          <div className="grid">
+            <h5>Next Oil Change Mileage: {CarDetails.nextOilChangeMileage}</h5>
+            <form onSubmit={this.props.changeNextOilMileage}>
+              <input type="hidden" name="carid" value={CarDetails.id} />
+              <input type="number" name="changeNextOilMileage" />
+              <input type="submit" value="Change Next Oil Change Mileage" />
+            </form>
+          </div>
+          <div className="grid">
+            <h5>
+              Last Tire Rotation Mileage: {CarDetails.lastTireRotationMileage}
+            </h5>
+            <form onSubmit={this.props.changeLastRotation}>
+              <input type="hidden" name="carid" value={CarDetails.id} />
+              <input type="number" name="changeLastRotation" />
+              <input type="submit" value="Change Last Tire Rotation Mileage" />
+            </form>
+          </div>
+          <div className="grid">
+            <h5>
+              Next Tire Rotation Mileage: {CarDetails.nextTireRotationMileage}
+            </h5>
+            <form onSubmit={this.props.changeNextRotation}>
+              <input type="hidden" name="carid" value={CarDetails.id} />
+              <input type="number" name="changeNextRotation" />
+              <input type="submit" value="Change Next Tire Rotation Mileage" />
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
